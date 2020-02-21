905a53607d0d9ce40947876323a06149e232e846
Josh Beglan <josh_beglan@yahoo.com>
2020-02-21 14:24:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    var musician = musicians[i];
+    var instrument = instruments[i];
+    array.push(musician + " plays " + instrument);
+  }
+  
+  return array;
+}
